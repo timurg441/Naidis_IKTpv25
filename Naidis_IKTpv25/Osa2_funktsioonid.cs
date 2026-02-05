fc41c5def8c1145b8694ada13eb0c9d44f6b00e1
opilane <opilane@HP-CZC2349HT4>
2026-02-05 14:04:26 +0200

    .

---
+++ Naidis_IKTpv25/Osa2_funktsioonid.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Naidis_IKTpv25
+{
+    public class Osa2_funktsioonid
+    {
+
+        public static string Juku(string nimi = "Juku")
+        {
+            string otsus = "";
+            string vastus = "";
+            int vanus = 0;
+            string pilet = "";
+            Console.WriteLine(nimi);
+            if (nimi.ToLower() == "juku")
+            {
+                Console.Write("Kui vana sa oled?: ");
+                try
+                {
+                    vanus = int.Parse(Console.ReadLine());
+                    if (vanus > 0 && vanus < 100)
+                    {
+                        if (vanus < 6)
+                        {
+                            pilet = "Sulle on kinopilet tasuta!";
+                        }
+                        else if (vanus >= 6 && vanus < 14)
+                        {
+                            pilet = "Sulle on kinopilet on lastepilet!";
+                        }
+                        else if (vanus >= 14 && vanus < 65)
+                        {
+                            pilet = "Sulle on kinopilet täispilet!";
+                        }
+                        else if (vanus >= 65)
+                        {
+                            pilet = "Sulle on kinopilet sooduspilet!";
+                        }
+                    }
+                    else
+                    {
+                        pilet = "Tundmatupilet. Vanus peab olema >0 arv ja <100!";
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                otsus = $"Lähme kinno! {pilet}";
+            }
+            else
+            {
+                otsus = "Ma olen hõivatud";
+            }
+            vastus = $"Tere {nimi}, {otsus}";
+            return vastus;
+        }
+    }
+}
--- Naidis_IKTpv25/StartPage.cs
+++ Naidis_IKTpv25/StartPage.cs
@@ -9,23 +9,23 @@ namespace Naidis_IKTpv25
         public static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("1. Osa Andmetüübid");
-            // Console.BackgroundColor = ConsoleColor.Green;
-            // Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("1. Osa Andmetüübid, Alamfunktsioonid/meetodid");
+            //Console.BackgroundColor= ConsoleColor.Green;
+            //Console.ForegroundColor= ConsoleColor.Blue;
             string tekst = "Tere tulemast C#-i maailma!";
-            Console.WriteLine($"Oli loodud muutja tekst, mis võrdub : {tekst}");
-            Console.WriteLine("Oli loodud muutja tekst, mis võrdub: {0}", tekst);
-            /* Console.WriteLine("Mis on sinu nimi?: ");
+            Console.WriteLine($"Oli loodud muutuja tekst, mis võrdub: {tekst}");
+            Console.WriteLine("Oli loodud muutuja tekst, mis võrdub: {0}", tekst);
+            Console.Write("Mis on sinu nimi?: ");
             string nimi = Console.ReadLine();
-            Console.Write("Kui vana sa oled?: ");
+            /*Console.Write("Kui vana sa oled?: ");
             try
             {
                 int vanus = int.Parse(Console.ReadLine());
-                Console.WriteLine($"Tere {nimi}. Sa oled {vanus} aastat vana.");
+                Console.WriteLine($"Tere {nimi}. Sa oled {vanus} aastat vana");
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                Console.WriteLine(e);              
             }
             try
             {
@@ -33,21 +33,30 @@ namespace Naidis_IKTpv25
                 float a = float.Parse(Console.ReadLine());
                 Console.Write("Arv 2: ");
                 float b = float.Parse(Console.ReadLine());
-                float vastus = Naidis_Funktsioonid.Summa(a, b);
+                float vastus = Naidis_funktsioonid.Summa(a, b);
                 Console.WriteLine($"Summa {a} ja {b} võrdub {vastus}");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
-            }
+            }*/
 
             Random rnd = new Random();
             int juhuslik_arv = rnd.Next(-5, 25);
-            Console.WriteLine(Naidis_Funktsioonid.Kuu_nimetus(juhuslik_arv));
+            //1.
+            Console.WriteLine(Naidis_funktsioonid.Kuu_nimetus(juhuslik_arv));
             juhuslik_arv = rnd.Next(-5, 25);
-            tekst = Naidis_Funktsioonid.Kuu_nimetus(juhuslik_arv);
+            //2. 
+            tekst = Naidis_funktsioonid.Kuu_nimetus(juhuslik_arv);
             Console.WriteLine(tekst);
+            //Juku ja kino
+            Console.Write(Osa2_funktsioonid.Juku());//vaikimisi nimi= juku
+            Console.Write(Osa2_funktsioonid.Juku(nimi));
+            Console.Write(Osa2_funktsioonid.Juku("Python"));
 
         }
+
+
+
     }
 }
