6ffd7aa813c1999c43fe7e41bde0d2aec4c4da37
Timur <1ame.xyz07@gmail.com>
2026-02-11 14:11:10 +0200

    .

---
+++ Naidis_IKTpv25/Isik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Naidis_IKTpv25
+{
+    public class Isik
+    {
+        public string Nimi;
+        public int Vanus;
+        public Isik(string nimi, int vanus)
+        {
+             Nimi = nimi;
+             Vanus = vanus;
+        }
+        public Isik() { }
+        public void Tervita()
+        {
+            Console.WriteLine($"Tere, minu nimi on {Nimi} ja ma olen {Vanus} aastat vana.");
+        }
+    }
+}
