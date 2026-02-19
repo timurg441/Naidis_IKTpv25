01e1dce8b4d93c3807dd9e772848e0bc59717ebb
Timur <1ame.xyz07@gmail.com>
2026-02-19 12:53:31 +0200

    .

---
+++ Naidis_IKTpv25/Inimene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Naidis_IKTpv25
+{
+    public class Inimene
+    {
+        public string Nimi;
+        public int Vanus;
+        public Inimene(string nimi, int vanus) 
+        { 
+            Nimi = nimi;
+            Vanus = vanus;
+        }
+    }
+}
